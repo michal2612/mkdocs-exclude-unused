252384d195324da628b66bcb48ee76a31f6cca2f
michal2612 <retrospektywny@gmail.com>
2024-10-12 15:20:45 +0100

    Release for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(name):
 
 setuptools.setup(
     name='mkdocs-exclude-unused',
-    version='1.0.0',
+    version='1.0.1',
     packages=['mkdocs_exclude_unused'],
     url='https://github.com/michal2612/mkdocs-exclude-unused',
     license='MIT',
